8c3f24b13eed1bbc0f1ca1400f331ea4ee8e1a1c
Minardi <nardiminardi20@gmail.com>
2024-06-10 17:41:28 +0700

    update lg

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const APP_NAME = "app.dubair.ai";
 const APP_VERSION = "0.0.1";
 const APP_DESCRIPTION = "Dubair is a simple, fast, and lightweight web browser for the desktop.";
-const API_URL = "http://20.243.18.160";
+const API_URL = "https://alfikri.tech";
 // const API_URL = "http://localhost:5000";
 const API_KEY_FIREBASE = "AIzaSyAvS4kWKBn0BRQCG63LY4AiCgLj7VK0-4A";
 const NODE_ENV = "development";
